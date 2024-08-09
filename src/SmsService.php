f666ce8c4aec74f516c09b21d862430a5aa4448f
sunyonghong <syh@sunyonghong.com>
2024-08-09 14:24:12 +0800

    优化: 路由地址

--- src/SmsService.php
+++ src/SmsService.php
@@ -11,11 +11,11 @@ class SmsService extends Service
     {
         $this->registerRoutes(function () {
             /** 接口路由--需要移入到composer中 */
-            Route::group('system/package/sms', function () {
+            Route::group('admin/system/package/sms', function () {
                 Route::get('/list', '@index')->name('system.package.sms'); // 短信配置页面
                 Route::get('/<provider>/config', '@config')->pattern(['provider' => '[a-zA-Z_]+'])->name('system.package.sms.config'); // 短信账号配置页面
                 Route::get('/verify', '@verify')->name('system.package.sms.config');
-            })->prefix('\eduline\sms\admin\service\Config')->middleware(['adminRoute']);
+            })->prefix('\eduline\sms\admin\service\Config')->middleware(['adminRoute', 'init', 'bindLoginUser']);
         });
     }
 }
